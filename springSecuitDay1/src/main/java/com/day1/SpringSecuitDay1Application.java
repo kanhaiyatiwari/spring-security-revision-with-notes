610cf2ef11a219952e517604437d13f800bd5ce5
kanhaiyatiwari <tiwarikanhaiya149@gmail.com>
2023-08-18 14:51:03 +0530

    add

---
+++ springSecuitDay1/src/main/java/com/day1/SpringSecuitDay1Application.java
@@ -0,0 +1,13 @@
+package com.day1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuitDay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuitDay1Application.class, args);
+	}
+
+}
